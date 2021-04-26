13798566aad6d9208cd07a84e4eac4d0012cf8a0
MGubernick <MGubernick61@gmail.com>
2021-04-26 13:58:48 -0500

    Spelling on Home page
    - corrected the spelling on the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,7 @@ class HomePage extends Component {
     return (
       <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '50px' }}>
         <h3 className="home-page-topline" style={{ fontSize: '60px', marginTop: '200px' }}>Welcome To Bridget Doherty&apos;s Pottery Website!</h3>
-        <h6 className="home-page-topline" style={{ alignSelf: 'center', fontSize: '40px', marginTop: '20px' }}>Take a look around and find your new favorite piece!</h6>
+        <h6 className="home-page-topline" style={{ alignSelf: 'center', fontSize: '40px', marginTop: '20px' }}>Take a look around and find your new favorite piece of pottery!</h6>
         {/* <img src="" alt="Home Page Image" width="500" height="600"> */}
       </div>
     )
